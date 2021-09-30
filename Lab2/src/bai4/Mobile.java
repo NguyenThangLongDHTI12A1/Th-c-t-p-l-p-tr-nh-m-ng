8d4d08e2cd81a09520f1d4d2ac0e9b5e528c82ec
NguyenThangLongDHTI12A1 <91250329+NguyenThangLongDHTI12A1@users.noreply.github.com>
2021-09-30 22:18:53 +0700

    Add files via upload

---
+++ Lab2/src/bai4/Mobile.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bai4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hailo
+ */
+public class Mobile extends Engine{
+    String country;
+    Scanner scmobile = new Scanner(System.in);
+
+    public Mobile() {
+    }
+
+    public Mobile(String country) {
+        this.country = country;
+    }
+
+    public Mobile(String country, String engineId, String engineName, String manufacturer, int yearMaking, float price) {
+        super(engineId, engineName, manufacturer, yearMaking, price);
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+    /**
+     *
+     */
+    @Override
+    public void input() {
+        super.input();
+        System.out.print("nhap nuoc san xuat: ");
+        this.country = scmobile.nextLine();
+    }
+    @Override
+    public void display() {
+        super.display();
+        System.out.print("nuoc san xuat: " + country + "\n");
+    } 
+}
